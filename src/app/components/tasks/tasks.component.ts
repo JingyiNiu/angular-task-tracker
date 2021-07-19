0ca87bccdb64c458f0ae64963e4f9f5cd918c388
JingyiNiu <niu.jy1203@gmail.com>
2021-07-19 16:53:28 +1200

    set up toggleReminder and save change to dbjson

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -16,12 +16,17 @@ export class TasksComponent implements OnInit {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
-  // run function to delete from server then filter at ui
+  // run function to delete from server then filter it at ui
   deleteTask(task: Task) {
     this.taskService
       .deleteTask(task)
       .subscribe(
         () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
       );
+  }
+
+  toggleReminder(task: Task) {
+    task.reminder = !task.reminder;
+    this.taskService.updateTaskReminder(task).subscribe();
   }
 }
