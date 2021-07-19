c196c675e0923510ad0a87c99e7b483ccdea02f6
JingyiNiu <niu.jy1203@gmail.com>
2021-07-19 21:25:36 +1200

    create nav and footer components and impelement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
 import { TasksComponent } from './components/tasks/tasks.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
 import { AddTaskComponent } from './components/add-task/add-task.component';
-import { FormStyle } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
+
+const AppRoutes: Routes = [
+  { path: '', component: TasksComponent },
+  { path: 'about', component: AboutComponent },
+];
 
 @NgModule({
   declarations: [
@@ -21,6 +29,9 @@ import { FormStyle } from '@angular/common';
     TasksComponent,
     TaskItemComponent,
     AddTaskComponent,
+    AboutComponent,
+    FooterComponent,
+    NavComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,6 +39,7 @@ import { FormStyle } from '@angular/common';
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
+    RouterModule.forRoot(AppRoutes, { enableTracing: true }),
   ],
   providers: [],
   bootstrap: [AppComponent],
