b9a1986cd62c1317575c5ac0ab773891a44917f8
JingyiNiu <niu.jy1203@gmail.com>
2021-07-19 15:00:34 +1200

    impletement delete task item

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -15,4 +15,13 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
+
+  // run function to delete from server then filter at ui
+  deleteTask(task: Task) {
+    this.taskService
+      .deleteTask(task)
+      .subscribe(
+        () => (this.tasks = this.tasks.filter((t) => t.id !== task.id))
+      );
+  }
 }
